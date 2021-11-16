8d069e3cab0976b98c80c3d3507a7f69ad24a106
Loris Prataiolo <73251774+Niotahely@users.noreply.github.com>
2021-11-16 15:08:34 +0100

    Create FirstEx_Returns_ProductAll_integers.ml
    first Exercise :  returns the product of all integers contained in the ls list.

---
+++ FirstEx_Returns_ProductAll_integers.ml
@@ -0,0 +1,14 @@
+
+(* first Exercise :  returns the product of all integers contained in the ls list. *)
+
+(*first version with pattern matching method*)
+
+let rec prod ls = match ls with (* computes the sum of the elements of a list *)
+    hd::tl -> hd * prod tl (* inductive case *)
+  | _ -> 1;; (* base case *)
+             
+(* Second Version *)            
+             
+let rec prod  = function (* computes the sum of the elements of a list *)
+    hd::tl -> hd * prod tl (* inductive case *)
+  | _ -> 1;; (* base case  *)
