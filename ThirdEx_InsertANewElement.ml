40424546759cb910a2f77fc68d1d6b6de0c0c115
Loris Prataiolo <73251774+Niotahely@users.noreply.github.com>
2021-11-16 16:52:45 +0100

    Create ThirdEx_InsertANewElement.ml
    inserts a new element ( el ) in the list ( ls ) returns the list obtained by adding el to the bottom of the list ls if el does not already belong to ls; returns ls otherwise

---
+++ ThirdEx_InsertANewElement.ml
@@ -0,0 +1,11 @@
+(* Third Exercise : insert el ls returns the list obtained by adding el to the bottom of the list ls 
+                if el does not already belong to ls; returns ls otherwise.*)
+
+let rec insert el ls  = match ls with
+  
+    []->[el] (* base case if the list is empty*) 
+          
+   (* otherwise it checks that el is not equal to the element of the list, if it is equal then it returns the list itself. 
+              otherwise it recursively calls the function to the end *) 
+
+  | hd::tl -> if el = hd then ls else hd :: (insert el tl  );;
