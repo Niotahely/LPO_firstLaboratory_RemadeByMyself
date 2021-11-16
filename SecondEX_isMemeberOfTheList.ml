ce9b3c5b866e7623f1afe94da806697ec05577a4
Loris Prataiolo <73251774+Niotahely@users.noreply.github.com>
2021-11-16 16:31:48 +0100

    Create SecondEX_isMemeberOfTheList.ml
    Second Exercise : member el ls returns true if and only if el is an element of the ls list.

---
+++ SecondEX_isMemeberOfTheList.ml
@@ -0,0 +1,8 @@
+
+(*Second Exercise : member el ls returns true if and only if el is an element of the ls list. *)
+
+let rec member el ls = match ls with
+        [] -> false    (* base case : when ls is empty *)
+        | hd::tl -> if hd = el then true else member el tl;;
+        
+        (*   ─=≡Σ((( つ•̀ω•́)つ   *)
